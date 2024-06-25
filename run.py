43d692299625b932ee1d359c80d8514cadc980d7
AdrianMrx <adrian.marxreiter@hs-augsburg.de>
2024-06-25 23:29:59 +0200

    add preliminary chat-design and add database

--- run.py
+++ run.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.String(200), nullable=True)
+    content = db.Column(db.String(500), nullable=True)
+    created_at = db.Column(db.DateTime(), default=datetime.utcnow)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def start_page():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
