9c238d65348704010831830436d2f5e7f40a78da
AdrianMrx <adrian.marxreiter@hs-augsburg.de>
2024-07-05 01:40:42 +0200

    remove unnecessary print

--- chat/events.py
+++ chat/events.py
@@ -8,7 +8,6 @@ socketio_blueprint = Blueprint('socketio', __name__)
 
 @socketio.on("send_message")
 def handle_send_message():
-    print("New message")
     messages_from_db = Message.query.order_by(Message.created_at).all()
     messages_dtos = [convert_message_to_messagedto(message).to_dict() for message in messages_from_db]
     emit("receive_message", {"messages": messages_dtos}, broadcast=True)
